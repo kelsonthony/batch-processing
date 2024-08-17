88dc62af4a45d7eda4dd6402bcd9739e6151e385
Kelson Anthony De Menezes <kelsonthony@MacBook-Air-de-Kelson.local>
2024-08-17 10:34:36 -0300

    update

--- src/main/java/com/kelsonthony/batchprocessing/entity/Customer.java
+++ src/main/java/com/kelsonthony/batchprocessing/entity/Customer.java
@@ -16,8 +16,8 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
+
     @Id
-    @Column(name = "CUSTOMER_ID")
     private int id;
     @Column(name = "FIRST_NAME")
     private String firstname;
